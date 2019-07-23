d24660eefc7ab0bf96bd2af5feaaa8cb0db2e1a4
jf-2020 <jcf129er@gmail.com>
2019-07-23 18:08:29 -0400

    completed part 3 -> rendering components with subcomponents

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
-import { View, Image } from 'react-native';
+import { Text, View } from 'react-native';
 
-export default class Bananas extends Component {
+class Greeting extends Component {
     render() {
-        let pic = {
-            uri: "https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg"
-        };
         return (
-            <View style={{ alignItems: "center" }}>
-                <Image source={pic} style={{ width: 193, height: 110 }} />
+            <View style={{ alignItems: 'center' }}>
+                <Text>Hello {this.props.name}!</Text>
+            </View>
+        );
+    }
+}
+
+export default class LotsOfGreetings extends Component {
+    render() {
+        return (
+            <View style={{ alignItems: 'center', top: 50 }}>
+                <Greeting name="Rexxar" />
+                <Greeting name="Jaina" />
+                <Greeting name="Valeera" />
             </View>
         );
     }
